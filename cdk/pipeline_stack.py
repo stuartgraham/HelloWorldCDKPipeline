7241bad9741ae5b10a0d2f70b8e0a279f0f9413e
Stuart Graham <stuart@stuart-graham.com>
2021-03-12 16:05:28 +0000

    Update buildspec

--- cdk/pipeline_stack.py
+++ cdk/pipeline_stack.py
@@ -1,13 +1,17 @@
+from os import path
 from aws_cdk import (
     core,
     aws_ecr as ecr,
     aws_codebuild as codebuild
 )
+from .buildspec import buildspec as buildspec
 
 class PipelineStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        this_dir = path.dirname(__file__)
 
         # ECR
         ecr_repo = ecr.Repository(self, "HelloWorldRepo")
@@ -27,4 +31,18 @@ class PipelineStack(core.Stack):
             ]
         )
 
-        codebuild.Project(self, "HelloWorldBuildProject", source=github_source)
+        codebuild.Project(self, "HelloWorldBuildProject", 
+            source=github_source, 
+            build_spec=codebuild.BuildSpec.from_object(buildspec),
+            environment=codebuild.BuildEnvironment(build_image=codebuild.LinuxBuildImage.STANDARD_4_0,
+                privileged=True),        
+            environment_variables= {
+            'TEST': {'value': 'test'},
+            #'PACKAGE_BUCKET': {'value': artifactsBucket.bucket_name},
+            #'AWS_ACCOUNT_ID': {'value': self.account},
+            'IMAGE_REPO_NAME': {'value': f'{self.account}.dkr.ecr.{self.region}.amazonaws.com/{ecr_repo}'},
+            #'IMAGE_TAG': {'value': tag}, 
+            }            
+            )
+
+

2f0c671e78923273478361ed091a3bf67e157d34
Stuart Graham <stuart@stuart-graham.com>
2021-03-14 22:21:59 +0000

    Initial commit

--- cdk/pipeline_stack.py
+++ cdk/pipeline_stack.py
@@ -7,6 +7,8 @@ from aws_cdk import (
     aws_ecs as ecs,
     aws_codebuild as codebuild,
     aws_codedeploy as codedeploy,
+    aws_codepipeline as codepipeline,
+    aws_codepipeline_actions as codepipeline_actions,
     aws_ec2 as ec2,
     aws_iam as iam,
     aws_elasticloadbalancingv2 as elb,
@@ -28,23 +30,26 @@ class PipelineStack(core.Stack):
         )
         ecr_repo.add_lifecycle_rule(max_image_count=10)
 
-        # Codebuild
-        codebuild.GitHubSourceCredentials(self, "CodeBuildGitHubCreds",
-            access_token=core.SecretValue.secrets_manager("github-token")
-        )
-        
-        github_source = codebuild.Source.git_hub(
-            owner="stuartgraham",
-            repo="HelloWorldCodePipeline",
-            webhook=True,
-            webhook_triggers_batch_build=False,
-            webhook_filters=[
-                codebuild.FilterGroup.in_event_of(codebuild.EventAction.PUSH)
-            ]
+        # Codepipeline
+        pipeline = codepipeline.Pipeline(self, "HelloWorldPipeline",
+            pipeline_name="HelloWorldPipeline",
         )
 
-        helloworld_codebuild_project = codebuild.Project(self, "HelloWorldBuildProject", 
-            source=github_source, 
+        github_source_artifact = codepipeline.Artifact()
+        source_action = codepipeline_actions.GitHubSourceAction(
+            action_name="GitHub_Source",
+            owner="stuartgraham",
+            repo="HelloWorldCodePipeline",
+            oauth_token=core.SecretValue.secrets_manager("github-token"),
+            output=github_source_artifact,
+            branch="main"
+        )
+        pipeline.add_stage(
+            stage_name="Source",
+            actions=[source_action]
+        )
+
+        codebuild_project = codebuild.PipelineProject(self, "HelloWorldBuildProject", 
             build_spec=codebuild.BuildSpec.from_object(buildspec),
             environment=codebuild.BuildEnvironment(build_image=codebuild.LinuxBuildImage.STANDARD_5_0,
                 privileged=True),        
@@ -54,18 +59,31 @@ class PipelineStack(core.Stack):
             }            
             )
 
-        helloworld_codebuild_project.role.add_managed_policy(
+        codebuild_project.role.add_managed_policy(
                 iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2ContainerRegistryPowerUser"))
 
+        build_action = codepipeline_actions.CodeBuildAction(
+            action_name="CodeBuild",
+            project=codebuild_project,
+            input=github_source_artifact,
+            outputs=[codepipeline.Artifact()],
+            execute_batch_build=True
+        )
+
+        pipeline.add_stage(
+            stage_name="Build",
+            actions=[build_action]
+        )
+
         # Codedeploy
-        hello_world_application = codedeploy.EcsApplication(self, "HelloWorldApplication",
+        application = codedeploy.EcsApplication(self, "HelloWorldApplication",
             application_name="HelloWorld"
         )
 
-        hello_world_codedeloy_role  = iam.Role(self, "HelloWorldCodeDeployRole",
+        codedeloy_role  = iam.Role(self, "HelloWorldCodeDeployRole",
             assumed_by=iam.ServicePrincipal("codedeploy.amazonaws.com"))
 
-        hello_world_codedeloy_role.add_managed_policy(
+        codedeloy_role.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodeDeployRoleForECS")
         )
 
@@ -74,76 +92,79 @@ class PipelineStack(core.Stack):
         target_vpc = ec2.Vpc.from_lookup(self, "VPC", vpc_name="Main VPC")
 
         # ALB
-        helloworld_alb = elb.ApplicationLoadBalancer(self, "HelloWorldALB",
+        alb = elb.ApplicationLoadBalancer(self, "HelloWorldALB",
             vpc=target_vpc,
             internet_facing=True
         )
 
-        helloworld_listener_1 = helloworld_alb.add_listener("HelloWorldListener1",
+        listener_1 = alb.add_listener("HelloWorldListener1",
             port=80,
             open=True
         )
 
-        helloworld_listener_2 = helloworld_alb.add_listener("HelloWorldListener2",
+        listener_2 = alb.add_listener("HelloWorldListener2",
             port=8080,
             open=True
         )
 
-        helloworld_tg1 = elb.ApplicationTargetGroup(self, "HelloWorldTG1",
+        tg1 = elb.ApplicationTargetGroup(self, "HelloWorldTG1",
             target_type=elb.TargetType.IP,
             port=5000,
             protocol=elb.ApplicationProtocol("HTTP"),
             vpc=target_vpc
         )
 
-        helloworld_tg2 = elb.ApplicationTargetGroup(self, "HelloWorldTG2",
+        tg2 = elb.ApplicationTargetGroup(self, "HelloWorldTG2",
             target_type=elb.TargetType.IP,
             port=5000,
             protocol=elb.ApplicationProtocol("HTTP"),
             vpc=target_vpc
         )
 
-        helloworld_listener_1.add_target_groups("HelloWorldAddTG1",
-            target_groups=[helloworld_tg1]
+        listener_1.add_target_groups("HelloWorldAddTG1",
+            target_groups=[tg1]
         )
 
-        helloworld_listener_2.add_target_groups("HelloWorldAddTG2",
-            target_groups=[helloworld_tg2]
+        listener_2.add_target_groups("HelloWorldAddTG2",
+            target_groups=[tg2]
         )
 
 
 
         # ECS
-        hello_world_ecs_cluster = ecs.Cluster(self, "HelloWorldEcsCluster",
+        ecs_cluster = ecs.Cluster(self, "HelloWorldEcsCluster",
             cluster_name="HelloWorldCluster",
             capacity_providers=["FARGATE", "FARGATE_SPOT"],
             vpc=target_vpc
         )
 
         # ECS Task
-        hello_world_ecs_exec_role  = iam.Role(self, "FargateContainerExecutionRole",
+        ecs_exec_role  = iam.Role(self, "FargateContainerExecutionRole",
             assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"))
 
-        hello_world_ecs_task_role  = iam.Role(self, "FargateContainerTaskRole",
+        ecs_task_role  = iam.Role(self, "FargateContainerTaskRole",
             assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"))
         
-        hello_world_task_def = ecs.FargateTaskDefinition(self, "HelloWorldTaskDef",
+        task_def = ecs.FargateTaskDefinition(self, "HelloWorldTaskDef",
             memory_limit_mib=512,
             cpu=256,
-            execution_role=hello_world_ecs_exec_role, 
-            task_role=hello_world_ecs_task_role
+            execution_role=ecs_exec_role, 
+            task_role=ecs_task_role
         )
 
-        hello_world_container = hello_world_task_def.add_container("HelloWorld",
+        container = task_def.add_container("HelloWorld",
             image=ecs.ContainerImage.from_ecr_repository(ecr_repo)
         )
 
-        hello_world_security_group = ec2.SecurityGroup(
+        port_mapping = ecs.PortMapping(container_port=5000, host_port=5000)
+        container.add_port_mappings(port_mapping)
+
+        security_group = ec2.SecurityGroup(
             self, "HelloWorldContainerSG",
             vpc=target_vpc,
             allow_all_outbound=True
         )
-        hello_world_security_group.add_ingress_rule(
+        security_group.add_ingress_rule(
             ec2.Peer.any_ipv4(),
             ec2.Port.tcp(5000)
         )
@@ -159,18 +180,23 @@ class PipelineStack(core.Stack):
             weight=100
         )
 
+        code_deploy_deployment_controller = ecs.DeploymentController(
+            type=ecs.DeploymentControllerType.CODE_DEPLOY
+        )
 
-        hello_world_ecs_service = ecs.FargateService(self, "HelloWorldService",
-            cluster=hello_world_ecs_cluster,
-            task_definition=hello_world_task_def,
+        ecs_service = ecs.FargateService(self, "HelloWorldService",
+            cluster=ecs_cluster,
+            task_definition=task_def,
             desired_count=5,
             assign_public_ip=False,
-            security_groups=[hello_world_security_group],
+            security_groups=[security_group],
             capacity_provider_strategies=[
                 fargate_spot_capacity_provider_strategy,
                 fargate_capacity_provider_strategy
-            ]
+            ],
+            deployment_controller=code_deploy_deployment_controller
         )
+        ecs_service.attach_to_application_target_group(tg1)
 
 
 
